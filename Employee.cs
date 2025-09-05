fa367b4f108eaa26649aae61e5e43e1d0e4c263f
dth235716-PhamQuangUyenNhi <nhi_dth235716@student.agu.edu.vn>
2025-09-05 16:14:26 +0700

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai6
+{
+    class Employee
+    {
+        private int id;
+        private string name;
+        private int yearofbirth;
+        private double bacluong;
+        private double luongcb;
+
+        //Hàm khởi tạo không có tham số
+        public Employee()
+        {
+            Id = 0;
+            Name = " ";
+            YearofBirth = 0;
+            Bacluong = 0;
+            Luongcb = 3000000;
+        }
+
+        //Các phương thức Properties để get/set giá trị cho các thuộc tính
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int YearofBirth
+        {
+            get { return yearofbirth; }
+            set { yearofbirth = value; }
+        }
+        public double Bacluong
+        {
+            get { return bacluong; }
+            set { bacluong = value; }
+        }
+        public double Luongcb
+        {
+            get { return luongcb; }
+            set { luongcb = value; }
+        }
+
+        //Các phương thức nhập/xuất dữ liệu
+        public void nhap()
+        {
+            Console.Write(" \t -Nhap ma so nhan vien:");
+            Id = int.Parse(Console.ReadLine());
+
+            Console.Write(" \t -Nhap ho ten:");
+            Name = Console.ReadLine();
+            
+            Console.Write(" \t -Nhap nam sinh:");
+            YearofBirth = int.Parse(Console.ReadLine());
+
+            double temp;
+            Console.Write(" \t -Nhap bac luong:");
+            temp = double.Parse(Console.ReadLine());
+            if (temp > 4 || temp < 1)
+            {
+                Console.WriteLine(" \t -Bac luong nv nam trong khoang 1 -> 4");
+                Console.Write(" \t -Vui long nhap lai:");
+                temp = double.Parse(Console.ReadLine());
+            }
+            Bacluong = temp;
+        }
+        public void xuat()
+        {
+            Console.WriteLine("{0,-15}{1,-15}{2,-15}{3,-15}", Id, Name, YearofBirth, Luongcb);
+        }
+
+        public double SetSalaryLevel()
+        {
+
+        }
+        GetIncome()
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Bai6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    public static void Main()
+    {
+        int n;
+        Console.Write(" Nhap so luong nhan vien: ");
+        n = int.Parse(Console.ReadLine());
+        Employee[] mnv = new Employee[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(" Nhap thong tin nhan vien thu: " + (i + 1).ToString());
+            mnv[i] = new Employee();
+            mnv[i].nhap();
+        }
+        Console.WriteLine(" Danh sach nhan vien: ");
+        Console.WriteLine("{0,-15}{1,-15}{2,-15}{3,-15}{4,-15}", "Ma so", "Ho Ten", "Nam sinh", "Luong Co Ban", "Thu nhap");
+        for (int i = 0; i < n; i++)
+        {
+            mnv[i].xuat();
+        }
+        Console.ReadLine();
+    }
+}
